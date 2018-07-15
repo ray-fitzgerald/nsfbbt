60c4d41a88f9669961a432406f6b42456e7696e0
unknown <sutherlandpb@gmail.com>
2018-07-15 09:45:54 -0400

    added spring boot starter

---
+++ src/main/java/com/bbt/hackathon/nsfweb/NsfWebApplication.java
@@ -0,0 +1,12 @@
+package com.bbt.hackathon.nsfweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsfWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsfWebApplication.class, args);
+	}
+}
